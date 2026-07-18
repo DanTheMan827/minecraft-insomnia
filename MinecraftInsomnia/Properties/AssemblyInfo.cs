b5f68038428730ea84081030a249715fdf631926
Daniel <790119+DanTheMan827@users.noreply.github.com>
2026-07-18 09:42:46 -0500

    feat: add LICENSE file and update assembly information
    - Added LICENSE file with GNU General Public License details
    - Updated assembly title and product name to "MinecraftInsomnia"
    - Corrected copyright format in AssemblyInfo.cs

--- MinecraftInsomnia/Properties/AssemblyInfo.cs
+++ MinecraftInsomnia/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Minecraft-Insomnia")]
+[assembly: AssemblyTitle("MinecraftInsomnia")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Minecraft-Insomnia")]
-[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyProduct("MinecraftInsomnia")]
+[assembly: AssemblyCopyright("© 2026 DanTheMan827")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
